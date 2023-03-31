a6e957c50e8f863c6f9ade2c6392df18711cb38d
Hugo <hugocosta8c2@gmail.com>
2023-03-31 17:58:33 -0300

    understanding command line arguments

--- ImgBatch.cpp
+++ ImgBatch.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
 	setlocale(LC_ALL, "pt_BR");
 
-	std::cout << "Olá, Mundo" << std::endl;
+	// std::cout << "Olá, Mundo" << std::endl;
+	for (int i = 0; i < argc; ++i) {
+		std::cout << argv[i] << std::endl;
+	}
+
+	getchar();
 
 	return 0;
 }
