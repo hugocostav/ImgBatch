0e8a0f3e4158839d503bdb4e3b08d13ee966dd66
Hugo <hugocosta8c2@gmail.com>
2023-03-31 21:53:02 -0300

    argument parser class skeleton

--- ImgBatch.cpp
+++ ImgBatch.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
+#include <iomanip>
+
+class ArgumentParser {
+	public:
+		void RegisterFlag(const std::string& flag) {
+
+		}
+
+		bool GetFlag(const std::string& flag) {
+
+			return 0;
+		}
+
+		void Parse(int agrc, char* argv[]) {
+
+		}
+
+	private:
+};
 
 int main(int argc, char* argv[]) {
 	setlocale(LC_ALL, "pt_BR");
 
-	// std::cout << "Olá, Mundo" << std::endl;
-	for (int i = 0; i < argc; ++i) {
-		std::cout << argv[i] << std::endl;
-	}
+	ArgumentParser argParser;
+	argParser.RegisterFlag("rename");
+	argParser.RegisterFlag("convert");
+	argParser.RegisterFlag("resize");
+	argParser.RegisterFlag("scale");
+
+	argParser.Parse(argc, argv);
+
+	std::cout << std::boolalpha << "Rename : " << argParser.GetFlag("rename") << std::endl;
+	std::cout << "Convert : " << argParser.GetFlag("convert") << std::endl;
+	std::cout << "Resize : " << argParser.GetFlag("resize") << std::endl;
+	std::cout << "Scale : " << argParser.GetFlag("scale") << std::endl;
+
 
 	getchar();
 
